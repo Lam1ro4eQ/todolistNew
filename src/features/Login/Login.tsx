7910342ce73eea323e24d09cbcb3c989db2b040b
Lam1ro4eQ <alsajgushev@gmail.com>
2024-06-07 12:45:02 +0300

    formik

--- src/features/Login/Login.tsx
+++ src/features/Login/Login.tsx
@@ -1,7 +1,21 @@
 import React from 'react';
 import {Button, Checkbox, FormControl, FormControlLabel, FormGroup, FormLabel, Grid, TextField} from "@mui/material";
+import {useFormik} from "formik";
 
 export const Login = () => {
+
+    const formik = useFormik({
+        initialValues: {
+            email: '',
+            password: '',
+            rememberMe: false
+        },
+        onSubmit: values => {
+            alert(JSON.stringify(values, null, 2));
+        },
+    });
+
+
     return <Grid container justifyContent={'center'}>
         <Grid item justifyContent={'center'}>
             <FormControl>
@@ -16,14 +30,29 @@ export const Login = () => {
                     <p>Email: free@samuraijs.com</p>
                     <p>Password: free</p>
                 </FormLabel>
-                <FormGroup>
-                    <TextField label='Email' margin='normal'/>
-                    <TextField type='password' margin='normal' label='Password'/>
-                    <FormControlLabel control={<Checkbox/>} label={'Remember me'}/>
-                    <Button type={'submit'} variant={'contained'} color={'primary'}>
-                        Login
-                    </Button>
-                </FormGroup>
+                <form onSubmit={formik.handleSubmit}>
+                    <FormGroup>
+                        <TextField
+                            label='Email'
+                            margin='normal'
+                            name={'email'}
+                            onChange={formik.handleChange}
+                            value={formik.values.email}
+                        />
+                        <TextField
+                            type='password'
+                            margin='normal'
+                            label='Password'
+                            name={'password'}
+                            onChange={formik.handleChange}
+                            value={formik.values.password}
+                        />
+                        <FormControlLabel control={<Checkbox name={'rememberMe'}/>} label={'Remember me'}/>
+                        <Button type={'submit'} variant={'contained'} color={'primary'}>
+                            Login
+                        </Button>
+                    </FormGroup>
+                    </form>
             </FormControl>
         </Grid>
     </Grid>
